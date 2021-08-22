e97d9a11b359e2b14aaa8b0a9e8913122c70952d
itspngu <hi@pngu.io>
2021-08-22 10:11:02 +0100

    add intro component for phrases

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error
-import { adverbs, verbs, adjectives, nouns } from "../buzzwords.yml";
+import { intros, adverbs, verbs, adjectives, nouns } from "../buzzwords.yml";
 
 function toTitleCase(str: string) {
     return str.replace(/\w\S*/g, function (txt) {
@@ -8,6 +8,7 @@ function toTitleCase(str: string) {
 }
 
 async function handleRequest(req: Request): Promise<Response> {
+    const random_intro = intros[Math.floor(Math.random() * intros.length)];
     const random_adverb = adverbs[Math.floor(Math.random() * adverbs.length)];
     const random_verb = verbs[Math.floor(Math.random() * verbs.length)];
     const random_adjective =
@@ -15,7 +16,7 @@ async function handleRequest(req: Request): Promise<Response> {
     const random_noun = nouns[Math.floor(Math.random() * nouns.length)];
 
     const phrase = toTitleCase(
-        `${random_adverb} ${random_verb} ${random_adjective} ${random_noun}`
+        `${random_intro} ${random_adverb} ${random_verb} ${random_adjective} ${random_noun}`
     );
 
     return new Response(JSON.stringify({ phrase: phrase }), {
